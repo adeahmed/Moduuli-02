919e60956d1d0fc776dae3964a1d3bed48299975
adeahmed <98258192+adeahmed@users.noreply.github.com>
2022-11-13 10:50:10 +0200

    j

--- Mod.04.js
+++ Mod.04.js
@@ -1,2 +1,17 @@
-//Write a program that asks the user for numbers until he gives zero. The given numbers are printed in the console from the largest to the smallest. 
+//Write a program that asks the user for numbers until he gives zero. 
+//The given numbers are printed in the console from the largest to the smallest
+
+var numbers = [];   //array for numbers
+var number = 1;     //variable for number
+
+while (number != 0) {
+    number = parseInt(prompt("Enter number: "));
+    numbers.push(number);
+}   //end of while
+
+numbers.sort(function (a, b) { return b - a }); //sort array from largest to smallest
+    
+
+
+
 

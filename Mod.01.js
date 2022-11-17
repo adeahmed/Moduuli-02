ea4bd24e3188998f7f504e5055504258ebf886a8
adeahmed <98258192+adeahmed@users.noreply.github.com>
2022-11-17 21:48:12 +0200

    k

--- Mod.01.js
+++ Mod.01.js
@@ -3,6 +3,7 @@ const numbers = [];
 for (let i = 0; i < 5; i++) {
     let number = +prompt("Enter number:");
     numbers.push(number);
+   
 }
 const result = document.querySelector('ul');
 result.innerHTML = numbers.sort((a,b) => a > b ? 1: -1).map(items => `<li>${items}</li>`).join(" ");
